982ef6e88a372ac1aa6d01cc99c48a86d88fb3c0
El Houssain Souhail <sou.ductive@gmail.com>
2024-02-28 00:24:08 +0100

    Fixed User view code

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -36,15 +36,16 @@ def delete_user(user_id):
 @app_views.route('/users', methods=['POST'])
 def create_user():
     """Creates a User."""
-    if not request.get_json:
-        abort(400, description="Not a JSON")
-    data = request.get_json
-    if 'email' not in data:
-        abort(400, description="Missing email")
-    if 'password' not in data:
-        abort(400, description="Missing password")
+    info = request.get_json(silent=True)
+    if not info:
+        abort(400, "Not a JSON")
+    if 'email' not in info:
+        abort(400, "Missing email")
+    if 'password' not in info:
+        abort(400, "Missing password")
     user = User(**data)
-    user.save()
+    storage.new(user)
+    storage.save()
     return jsonify(user.to_dict()), 201
 
 
@@ -54,12 +55,14 @@ def update_user(user_id):
     user = storage.get(User, user_id)
     if user is None:
         abort(404)
-    if not request.get_json:
-        abort(400, description="Not a JSON")
-    data = request.get_json
-    ignore_keys = ['id', 'email', 'created_at', 'updated_at']
-    for key, value in data.items():
-        if key not in ignore_keys:
+
+    info = request.get_json(silent=True)
+    if not info:
+        abort(400, "Not a JSON")
+
+    for key, value in info.items():
+        if key not in ['id', 'email', 'created_at', 'updated_at']:
             setattr(user, key, value)
+
     user.save()
     return jsonify(user.to_dict()), 200
