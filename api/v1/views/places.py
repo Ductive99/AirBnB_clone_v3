a5aa81e7e6a1d9f6ceb9d7f432b4721c4f8478cc
El Houssain Souhail <sou.ductive@gmail.com>
2024-02-28 00:48:28 +0100

    Fixed Place view's code

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -43,19 +43,22 @@ def create_place(city_id):
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-    if not request.get_json:
-        abort(400, description="Not a JSON")
-    data = request.get_json
-    if 'user_id' not in data:
-        abort(400, description="Missing user_id")
-    if 'name' not in data:
-        abort(400, description="Missing name")
-    user_id = data['user_id']
+
+    info = request.get_json(silent=True)
+    if not info:
+        abort(400, "Not a JSON")
+    if 'user_id' not in info:
+        abort(400, "Missing user_id")
+    if 'name' not in info:
+        abort(400, "Missing name")
+
+    user_id = info['user_id']
     if storage.get(User, user_id) is None:
         abort(404)
-    data['city_id'] = city_id
-    place = Place(**data)
-    place.save()
+    info['city_id'] = city_id
+    place = Place(**info)
+    storage.new(place)
+    storage.save()
     return jsonify(place.to_dict()), 201
 
 
@@ -65,12 +68,12 @@ def update_place(place_id):
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
-    if not request.get_json:
-        abort(400, description="Not a JSON")
-    data = request.get_json
-    ignore_keys = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
-    for key, value in data.items():
-        if key not in ignore_keys:
+    info = request.get_json(silent=True)
+    if not info:
+        abort(400, "Not a JSON")
+
+    for key, value in info.items():
+        if key not in ['id', 'user_id', 'city_id', 'created_at', 'updated_at']:
             setattr(place, key, value)
     place.save()
     return jsonify(place.to_dict()), 200
