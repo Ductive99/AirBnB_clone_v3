0653879e707ff9dd0d8cd89446f37cfdaf38fd0a
El Houssain Souhail <sou.ductive@gmail.com>
2024-02-27 01:18:05 +0100

    Seperated CRUD operations

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -25,26 +25,32 @@ def get_city(city_id):
     return jsonify(obj.to_dict())
 
 
-@app_views.route('/cities/<city_id>', methods=['DELETE', 'PUT'])
-def UD_city(city_id):
-    """Updates or Deletes a City object"""
-    obj = storage.get(City, city_id)
-    if not obj:
+@app_views.route('/cities/<city_id>', methods=['DELETE'])
+def delete_city(city_id):
+    """Deletes a City object."""
+    city = storage.get(City, city_id)
+    if not city:
         abort(404)
+    city.delete()
+    storage.save()
+    return jsonify({}), 204
 
-    if request.method == "DELETE":
-        obj.delete()
-        storage.save()
-        return jsonify({})
+
+@app_views.route('/cities/<city_id>', methods=['PUT'])
+def update_city(city_id):
+    """Updates a City object."""
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
 
     info = request.get_json(silent=True)
     if not info:
         abort(400, "Not a JSON")
     for k, v in info.items():
         if k not in ['id', 'created_at', 'updated_at', 'state_id']:
-            setattr(obj, k, v)
-    obj.save()
-    return jsonify(obj.to_dict())
+            setattr(city, k, v)
+    storage.save()
+    return jsonify(city.to_dict())
 
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'])
